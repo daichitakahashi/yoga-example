686f23d6fb6115c2a7bfca904b0924e086d121d1
daichitakahashi <t.daichi.snd@gmail.com>
2024-02-09 12:08:12 +0900

    feat: 環境変数の読み込みと、Nodeインターフェースの実装を試す

--- src/index.ts
+++ src/index.ts
@@ -8,25 +8,76 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+import { createYoga } from 'graphql-yoga';
+import SchemaBuilder from '@pothos/core';
+
 export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-	//
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	// MY_QUEUE: Queue;
+	DEFAULT_NAME: string;
 }
 
+// ビルダーに環境変数を渡してあげる。
+const builder = new SchemaBuilder<{ Context: Env }>({});
+
+const Node = builder.interfaceRef('Node').implement({
+	fields: (t) => ({
+		id: t.id(),
+	}),
+});
+
+interface UserShape {
+	id: string;
+	name: string;
+}
+const User = builder.objectRef<UserShape>('User').implement({
+	interfaces: [Node],
+	fields: (t) => ({
+		id: t.exposeID('id'),
+		name: t.exposeString('name'),
+	}),
+	isTypeOf: (v) => (typeof v === 'object' && v && 'id' in v && typeof v.id === 'string' && v.id.startsWith('User:')) || false,
+});
+
+builder.queryType({
+	fields: (t) => ({
+		node: t.field({
+			type: Node,
+			args: {
+				id: t.arg({
+					type: 'ID',
+					required: true,
+				}),
+			},
+			resolve: (parent, args, ctx) => {
+				return {
+					id: args.id,
+					name: 'Hey',
+				};
+			},
+		}),
+		user: t.field({
+			type: User,
+			args: {
+				id: t.arg({
+					type: 'String',
+					description: 'ユーザーID',
+					required: true,
+				}),
+			},
+			resolve: (parent, args, ctx) => {
+				return {
+					id: args.id,
+					name: ctx.DEFAULT_NAME, // 環境変数を参照できることを確認。
+					//contact: { email: 'peter@example.com' },
+				};
+			},
+		}),
+	}),
+});
+
+const gql = createYoga({
+	schema: builder.toSchema(),
+});
+
 export default {
-	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
-	},
+	fetch: gql.fetch,
 };
